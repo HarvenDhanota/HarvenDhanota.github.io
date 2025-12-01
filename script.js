ddad2e1c25e24f15c3343a83895bbb0e63ce05e3
HarvenDhanota <harvendhanota@hotmail.com>
2025-12-01 12:58:57 -0800

    Added sin, cos, ln functionality.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function appendValue(value) {
+function AppendValue(value) {
     document.getElementById('display').value += value;
 }
 
-function calculate() {
+function Calculate() {
     let display = document.getElementById('display');
     try {
         display.value = eval(display.value);
@@ -12,6 +12,36 @@ function calculate() {
     }
 }
 
-function clearDisplay() {
+function Sine() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(Math.sin(display.value));
+    }
+    catch {
+        display.value = "Error";
+    }
+}
+
+function Cosine() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(Math.cos(display.value));
+    }
+    catch {
+        display.value = "Error";
+    }
+}
+
+function Logarithm() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(Math.log(display.value));
+    }
+    catch {
+        display.value = "Error";
+    }
+}
+
+function ClearDisplay() {
     document.getElementById('display').value = "";
 }
